308089424a283adb3cb8f7b7d51b1e89f16b03cf
rmuchow95 <rachelmuchow95@gmail.com>
2022-11-05 14:52:26 -0500

    Made some changes, deleted some code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new expense tracker";
+  const title = "React Expense Tracker";
 
   return (
     <div className="App">
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,6 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Expense Tracker</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Expense</a>
